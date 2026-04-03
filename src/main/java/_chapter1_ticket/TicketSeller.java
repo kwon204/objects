b4063d34f00545e60fa6f962f870bd146d590fd8
kwon204 <dafeg3@naver.com>
2026-04-03 20:07:38 +0900

    티켓 판매원을 구현한다.
    - 관람객에게 현금이나 초대장을 받아 매표소에서 티켓으로 교환해준다.
    - 티켓으로 바꿔주기 위해 매표소를 알고 있어야 한다.

---
+++ src/main/java/_chapter1_ticket/TicketSeller.java
@@ -0,0 +1,13 @@
+package _chapter1_ticket;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
