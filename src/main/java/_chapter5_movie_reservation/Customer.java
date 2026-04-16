3e0f25e176fad81ba98e1362ea6db9268243ea33
kwon204 <dafeg3@naver.com>
2026-04-16 23:43:02 +0900

    영화를 예매하는 Screening을 구현하자
    - 예매에 대한 정보 전문가로 Reservation을 생성한다.
    - 요금 계산 메시지를 Movie에게 전송해서 가격을 구한다.

---
+++ src/main/java/_chapter5_movie_reservation/Customer.java
@@ -0,0 +1,11 @@
+package _chapter5_movie_reservation;
+
+public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
