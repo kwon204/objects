3d2a6d2b08c76c0ce80df60cc43215ec2aed7b81
kwon204 <dafeg3@naver.com>
2026-04-16 18:20:04 +0900

    DiscountCondition을 개선하자
    - 내부 상태에 대해 스스로 책임지는 객체로 만들자.
    - isDiscountable: 할인 조건으로 할인 여부를 판단한다

--- src/main/java/_chapter4_movie_reservation/DiscountCondition.java
+++ src/main/java/_chapter4_movie_reservation/DiscountCondition.java
@@ -12,43 +12,33 @@ public class DiscountCondition {
     private LocalTime startTime;
     private LocalTime endTime;
 
-    public DiscountConditionType getType() {
-        return type;
-    }
-
-    public void setType(DiscountConditionType type) {
+    public DiscountCondition(DiscountConditionType type, int sequence, DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
         this.type = type;
-    }
-
-    public int getSequence() {
-        return sequence;
-    }
-
-    public void setSequence(int sequence) {
         this.sequence = sequence;
-    }
-
-    public DayOfWeek getDayOfWeek() {
-        return dayOfWeek;
-    }
-
-    public void setDayOfWeek(DayOfWeek dayOfWeek) {
         this.dayOfWeek = dayOfWeek;
-    }
-
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(LocalTime startTime) {
         this.startTime = startTime;
-    }
-
-    public LocalTime getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(LocalTime endTime) {
         this.endTime = endTime;
+    }
+
+    public DiscountConditionType getType() {
+        return this.type;
+    }
+
+    public boolean isDiscountable(DayOfWeek dayOfWeek, LocalTime time) {
+        if (type != DiscountConditionType.PERIOD) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.dayOfWeek.equals(dayOfWeek) &&
+                !this.startTime.isAfter(time) &&
+                !this.endTime.isBefore(time);
+    }
+
+    public boolean isDiscountable(int sequence) {
+        if (type != DiscountConditionType.SEQUENCE) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.sequence == sequence;
     }
 }
