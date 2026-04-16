511ad6085fb85a9c9a3b5a2ce820df55755f4afc
kwon204 <dafeg3@naver.com>
2026-04-16 23:59:21 +0900

    영화 가격을 계산하는 Movie를 구현하자
    - 영화 가격과 할인 정책으로 가격을 계산할 수 있다.
    - 할인 주건에 충족하는지는 알지 못해서 보유한 할인 조건에 충족하는지
      메시지를 보낸다.

---
+++ src/main/java/_chapter5_movie_reservation/Movie.java
@@ -0,0 +1,54 @@
+package _chapter5_movie_reservation;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+    public Money calculateMovieFee(Screening screening) {
+        if (isDiscountable(screening)) {
+            return fee.minus(calculateDiscountAmount());
+        }
+    }
+
+    private boolean isDiscountable(Screening screening) {
+        return discountConditions.stream()
+                .anyMatch(condition -> condition.isSatisfiedBy(screening));
+    }
+
+    private Money calculateDiscountAmount() {
+        switch (movieType) {
+            case MovieType.AMOUNT_DISCOUNT -> {
+                return calculateAmountDiscountAmount();
+            }
+            case MovieType.PERCENT_DISCOUNT -> {
+                return calculatePercentDiscountAmount();
+            }
+            case MovieType.NONE_DISCOUNT -> {
+                return calculateNoneDiscountAmount();
+            }
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    private Money calculateAmountDiscountAmount() {
+        return discountAmount;
+    }
+
+    private Money calculatePercentDiscountAmount() {
+        return fee.times(discountPercent);
+    }
+
+    private Money calculateNoneDiscountAmount() {
+        return Money.ZERO;
+    }
+}
---
+++ src/main/java/_chapter5_movie_reservation/MovieType.java
@@ -0,0 +1,7 @@
+package _chapter5_movie_reservation;
+
+public enum MovieType {
+    AMOUNT_DISCOUNT,
+    PERCENT_DISCOUNT,
+    NONE_DISCOUNT,
+}
