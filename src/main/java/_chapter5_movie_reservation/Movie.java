7938880994c887e50a6e8a1bef3c46bab82c58c8
kwon204 <dafeg3@naver.com>
2026-04-30 17:33:59 +0900

    Movie 분리하기
    - 금액 할인 영화와 비율 할인 영화라는 두 타입을 Movie에서 구현하고 있다.
    - Movie는 구현을 공유해야 하기 때문에 추상 클래스로 만든다.
    - 각각 AmountDiscountMovie와 PercentDiscountMovie로 분리한다.

--- src/main/java/_chapter5_movie_reservation/Movie.java
+++ src/main/java/_chapter5_movie_reservation/Movie.java
@@ -1,17 +1,21 @@
 package _chapter5_movie_reservation;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
-public class Movie {
+public abstract class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
     private List<DiscountCondition> discountConditions;
 
-    private MovieType movieType;
-    private Money discountAmount;
-    private double discountPercent;
+    public Movie(String title, Duration runningTime, Money fee, DiscountCondition... discountConditions) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountConditions = Arrays.asList(discountConditions);
+    }
 
     public Money calculateMovieFee(Screening screening) {
         if (isDiscountable(screening)) {
@@ -26,31 +30,9 @@ public class Movie {
                 .anyMatch(condition -> condition.isSatisfiedBy(screening));
     }
 
-    private Money calculateDiscountAmount() {
-        switch (movieType) {
-            case MovieType.AMOUNT_DISCOUNT -> {
-                return calculateAmountDiscountAmount();
-            }
-            case MovieType.PERCENT_DISCOUNT -> {
-                return calculatePercentDiscountAmount();
-            }
-            case MovieType.NONE_DISCOUNT -> {
-                return calculateNoneDiscountAmount();
-            }
-        }
-
-        throw new IllegalArgumentException();
+    protected Money getFee() {
+        return this.fee;
     }
 
-    private Money calculateAmountDiscountAmount() {
-        return discountAmount;
-    }
-
-    private Money calculatePercentDiscountAmount() {
-        return fee.times(discountPercent);
-    }
-
-    private Money calculateNoneDiscountAmount() {
-        return Money.ZERO;
-    }
+    abstract protected Money calculateDiscountAmount();
 }
---
+++ src/main/java/_chapter5_movie_reservation/NoneDiscountMovie.java
@@ -0,0 +1,14 @@
+package _chapter5_movie_reservation;
+
+import java.time.Duration;
+
+public class NoneDiscountMovie extends Movie {
+    public NoneDiscountMovie(String title, Duration runningTime, Money fee, DiscountCondition... discountConditions) {
+        super(title, runningTime, fee, discountConditions);
+    }
+
+    @Override
+    protected Money calculateDiscountAmount() {
+        return Money.ZERO;
+    }
+}
