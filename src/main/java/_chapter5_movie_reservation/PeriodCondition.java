16e522145eb8a2b3218eb77dd3a940ed991d6664
kwon204 <dafeg3@naver.com>
2026-04-30 17:02:59 +0900

    DiscountCondition 타입 분리하기
    - 순번 조건과 기간 조건 두 개의 타입이 공존하는 DiscountCondition 분리
      - SequenceCondition
      - PeriodCondition
    - 인스턴스 초기화 시점에 따라 서로 다른 속성들을 초기화하면 응집도가
      낮다.
    - 메소드 그룹이 서로 다른 속성 그룹을 사용하면 응집도가 낮다.

---
+++ src/main/java/_chapter5_movie_reservation/PeriodCondition.java
@@ -0,0 +1,22 @@
+package _chapter5_movie_reservation;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class PeriodCondition {
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public PeriodCondition(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return dayOfWeek.equals(screening.getWhenScreened().getDayOfWeek()) &&
+                !startTime.isAfter(screening.getWhenScreened().toLocalTime()) &&
+                !endTime.isBefore(screening.getWhenScreened().toLocalTime());
+    }
+}
---
+++ src/main/java/_chapter5_movie_reservation/SequenceCondition.java
@@ -0,0 +1,13 @@
+package _chapter5_movie_reservation;
+
+public class SequenceCondition {
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return sequence == screening.getSequence();
+    }
+}
